75c27f6c50392751c373cad02f58420868d82d01
Sarowar Hosen <sarowarhosenakib2@gmail.com>
2021-01-22 18:49:05 +0600

    remote yarn.lock

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 //express middleware
 app.use(cors())
